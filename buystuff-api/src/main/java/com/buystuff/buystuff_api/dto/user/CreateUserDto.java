63b64b30bf02ae43675433fed67d28e6d2c83692
Prateik Lohani <lohaniprateik@gmail.com>
2025-12-30 16:16:07 +0530

    added: user dtos

---
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/dto/user/CreateUserDto.java
@@ -0,0 +1,16 @@
+package com.buystuff.buystuff_api.dto.user;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CreateUserDto {
+	@NotBlank private String name;
+	@NotNull private LocalDate dob;
+	@NotBlank private String phone;
+	@NotBlank private String countryCode;
+}
---
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/dto/user/UserDto.java
@@ -0,0 +1,18 @@
+package com.buystuff.buystuff_api.dto.user;
+
+import java.time.LocalDate;
+
+import com.buystuff.buystuff_api.abstract_classes.BaseResponseDto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserDto extends BaseResponseDto {
+	@NotBlank private String name;
+	@NotNull private LocalDate dob;
+	@NotBlank private String phone;
+	@NotBlank private String countryCode;
+}
