fe26c8b7bd17bb69990f5f5550d989da91c523d4
Prateik Lohani <lohaniprateik@gmail.com>
2026-01-12 16:39:28 +0530

    add: account id mapping for PK in users table

--- buystuff-api/src/main/java/com/buystuff/buystuff_api/entities/User.java
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/entities/User.java
@@ -3,17 +3,12 @@ package com.buystuff.buystuff_api.entities;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import jakarta.persistence.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import com.buystuff.buystuff_api.abstract_classes.BaseEntity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -31,6 +26,7 @@ public class User extends BaseEntity {
 	private UUID accountId;
 
 	@OneToOne(optional = false)
+    @MapsId
 	@JoinColumn(
 		name = "account_id",
 		referencedColumnName = "account_id",
