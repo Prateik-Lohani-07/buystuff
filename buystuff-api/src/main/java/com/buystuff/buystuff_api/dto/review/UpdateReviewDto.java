5977bfb6c116b8d2f495c29d82d78f4c3d556249
Prateik Lohani <lohaniprateik@gmail.com>
2026-02-04 10:49:52 +0530

    add: update review dto

---
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/dto/review/UpdateReviewDto.java
@@ -0,0 +1,17 @@
+package com.buystuff.buystuff_api.dto.review;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UpdateReviewDto {
+	@NotNull
+	private UUID reviewId;
+
+	private Integer rating;
+	private String title;
+	private String content;
+}
