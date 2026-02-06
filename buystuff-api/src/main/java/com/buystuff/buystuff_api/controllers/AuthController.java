c5d0d95357f75f685ce5aa5a547525632bd8bcd5
Prateik Lohani <lohaniprateik@gmail.com>
2026-02-06 14:48:54 +0530

    add: get current user info endpoint

--- buystuff-api/src/main/java/com/buystuff/buystuff_api/controllers/AuthController.java
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/controllers/AuthController.java
@@ -1,18 +1,26 @@
 package com.buystuff.buystuff_api.controllers;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.buystuff.buystuff_api.dto.ApiResponse;
+import com.buystuff.buystuff_api.dto.account.AccountDto;
 import com.buystuff.buystuff_api.dto.authentication.LoginDto;
 import com.buystuff.buystuff_api.dto.authentication.SignupDto;
+import com.buystuff.buystuff_api.entities.Account;
+import com.buystuff.buystuff_api.entities.UserPrincipal;
+import com.buystuff.buystuff_api.mappers.account.AccountMapper;
 import com.buystuff.buystuff_api.services.auth.AuthService;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
@@ -28,7 +36,11 @@ public class AuthController {
 		@Valid @RequestBody 
 		LoginDto loginDto
 	) {
+		log.info("START: login controller");
+		
 		String jwtToken = authService.authenticate(loginDto);
+		
+		log.info("END: login controller");
 		return ApiResponse.success("Login successful", jwtToken);
     }
 
@@ -37,19 +49,24 @@ public class AuthController {
 		@Valid @RequestBody 
 		SignupDto signupDto
 	) {
+		log.info("START: signup controller");
+		
 		String jwtToken = authService.registerUser(signupDto);
+
+		log.info("END: signup controller");
 		return ApiResponse.success("Signup successful", jwtToken);
     }
 
-    // @GetMapping("/me")
-    // public ApiResponse<String> getCurrentUser(
-	// 	@Valid @RequestBody 
-	// 	SignupDto signupDto
-	// ) {
-    //     try {
-    //         // return authService.registerUser(signupDto);
-    //     } catch (RuntimeException e) {
-    //         throw new RuntimeException(e);
-    //     }
-    // }
+    @GetMapping("/me")
+    public ApiResponse<AccountDto> getCurrentUser(
+		@AuthenticationPrincipal UserPrincipal userPrincipal
+	) {
+		log.info("START: getCurrentUser controller");
+		
+		Account account = userPrincipal.getAccount();
+		AccountDto accountDto = AccountMapper.toDto(account);
+		
+		log.info("END: getCurrentUser controller");
+		return ApiResponse.success("Successfully fetched account details", accountDto);
+    }
 }
