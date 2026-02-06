fd7dc61f4db6e335208e9d4542b460dfea6e89b2
Prateik Lohani <lohaniprateik@gmail.com>
2026-02-06 15:15:23 +0530

    add: validations in update user dto

--- buystuff-api/src/main/java/com/buystuff/buystuff_api/dto/user/UpdateUserDto.java
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/dto/user/UpdateUserDto.java
@@ -2,14 +2,23 @@ package com.buystuff.buystuff_api.dto.user;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class UpdateUserDto {
+	@Size(min = 3)
 	private String firstName;
+
+	@Size(min = 3)
 	private String lastName;
+	
 	private LocalDate dateOfBirth;
+	
+	@Size(min = 10)
 	private String phone;
+	
+	@Size(min = 1)
 	private String countryCode;
 }
