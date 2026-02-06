747e408838719fa0d7b8e049f6c6d139053c7339
Prateik Lohani <lohaniprateik@gmail.com>
2026-02-06 14:49:17 +0530

    add: account mapper

--- buystuff-api/src/main/java/com/buystuff/buystuff_api/dto/account/AccountDto.java
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/dto/account/AccountDto.java
@@ -1,5 +1,8 @@
 package com.buystuff.buystuff_api.dto.account;
 
+import java.util.UUID;
+
+import com.buystuff.buystuff_api.abstract_classes.BaseResponseDto;
 import com.buystuff.buystuff_api.dto.user.UserDto;
 import com.buystuff.buystuff_api.enums.Role;
 
@@ -7,12 +10,14 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-public class AccountDto {
+public class AccountDto extends BaseResponseDto {
+	@NotNull
+	private UUID accountId;
+
 	@Email
 	private String email;
 
---
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/mappers/account/AccountMapper.java
@@ -0,0 +1,41 @@
+package com.buystuff.buystuff_api.mappers.account;
+
+import com.buystuff.buystuff_api.dto.account.AccountDto;
+import com.buystuff.buystuff_api.dto.authentication.SignupDto;
+import com.buystuff.buystuff_api.dto.user.CreateUserDto;
+import com.buystuff.buystuff_api.entities.Account;
+import com.buystuff.buystuff_api.entities.User;
+import com.buystuff.buystuff_api.enums.Role;
+import com.buystuff.buystuff_api.mappers.user.UserMapper;
+
+public abstract class AccountMapper {
+	public static Account toEntity(SignupDto dto, String passwordHash) {
+		Account account = new Account();
+        String email = dto.getEmail();
+        CreateUserDto createUserDto = dto.getUserInfo();
+        
+		User user = UserMapper.toEntity(createUserDto, account);
+
+        account.setEmail(email);
+        account.setPasswordHash(passwordHash);
+        account.setUser(user);
+        account.setRole(Role.CUSTOMER);
+
+		return account;
+	}
+
+	public static AccountDto toDto(Account entity) {
+		AccountDto dto = new AccountDto();
+
+		dto.setAccountId(entity.getAccountId());
+
+		dto.setRole(entity.getRole());
+		dto.setEmail(entity.getEmail());
+		dto.setUser(UserMapper.toDTO(entity.getUser()));
+
+		dto.setCreatedAt(entity.getCreatedAt());
+		dto.setUpdatedAt(entity.getUpdatedAt());
+
+		return dto;
+	}
+}
