911be745fb4c893796fe214ee0f4ba9328fd4f3c
Prateik Lohani <lohaniprateik@gmail.com>
2026-02-09 11:10:00 +0530

    move: coupon dtos

--- buystuff-api/src/main/java/com/buystuff/buystuff_api/dto/cart/coupon/CouponDto.java
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/dto/cart/coupon/CouponDto.java
@@ -1,16 +1,19 @@
-package com.buystuff.buystuff_api.dto.coupon;
+package com.buystuff.buystuff_api.dto.cart.coupon;
 
+import java.util.UUID;
+
+import com.buystuff.buystuff_api.abstract_classes.BaseResponseDto;
 import com.buystuff.buystuff_api.enums.DiscountType;
 
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-public class CreateCouponDto {
+public class CouponDto extends BaseResponseDto {
+	@NotNull private UUID couponId;
 	@NotBlank private String couponCode;
 	@NotBlank private String name;
 	@DecimalMin(value = "0.0") private Double discountValue;
--- buystuff-api/src/main/java/com/buystuff/buystuff_api/dto/cart/coupon/CreateCouponDto.java
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/dto/cart/coupon/CreateCouponDto.java
@@ -1,20 +1,15 @@
-package com.buystuff.buystuff_api.dto.coupon;
+package com.buystuff.buystuff_api.dto.cart.coupon;
 
-import java.util.UUID;
-
-import com.buystuff.buystuff_api.abstract_classes.BaseResponseDto;
 import com.buystuff.buystuff_api.enums.DiscountType;
 
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-public class CouponDto extends BaseResponseDto {
-	@NotNull private UUID couponId;
+public class CreateCouponDto {
 	@NotBlank private String couponCode;
 	@NotBlank private String name;
 	@DecimalMin(value = "0.0") private Double discountValue;
