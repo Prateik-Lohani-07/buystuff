6c7f4df46f44946be6a86b4c26dfe9d1b0607877
Prateik Lohani <lohaniprateik@gmail.com>
2026-02-09 11:09:45 +0530

    update: rename update cart dto

---
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/dto/cart/InitializeCartDto.java
@@ -0,0 +1,24 @@
+package com.buystuff.buystuff_api.dto.cart;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.buystuff.buystuff_api.dto.cart.cart_item.CreateCartItemDto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class InitializeCartDto {
+	@NotNull
+	private UUID accountId;
+
+	@Valid
+	private List<CreateCartItemDto> items;
+
+	private UUID couponId;
+}
