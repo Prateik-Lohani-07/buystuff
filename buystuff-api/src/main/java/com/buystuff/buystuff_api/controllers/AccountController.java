2ebeba28720a6eea13c4c3449a3e6d6fa1d8bba1
Prateik Lohani <lohaniprateik@gmail.com>
2026-02-12 17:54:31 +0530

    chore: indentation fix

--- buystuff-api/src/main/java/com/buystuff/buystuff_api/controllers/AccountController.java
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/controllers/AccountController.java
@@ -8,10 +8,10 @@ import com.buystuff.buystuff_api.services.account.AccountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-	@RestController
-	@Slf4j
-	@RequestMapping("/api/v1/account")
-	@RequiredArgsConstructor
+@RestController
+@Slf4j
+@RequestMapping("/api/v1/account")
+@RequiredArgsConstructor
 public class AccountController {
 	private final AccountService accountService;
 
