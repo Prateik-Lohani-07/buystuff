461792baefe45589fe28639e65410a4c37f804db
Prateik Lohani <lohaniprateik@gmail.com>
2026-02-16 16:52:56 +0530

    add: order actions and updated order status

---
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/enums/OrderAction.java
@@ -0,0 +1,10 @@
+package com.buystuff.buystuff_api.enums;
+
+public enum OrderAction {
+	PAY,
+	SHIP,
+	DELIVER,
+	CANCEL,
+	RETURN,
+	RETURN_PERIOD_OVER;
+}
--- buystuff-api/src/main/java/com/buystuff/buystuff_api/enums/OrderStatus.java
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/enums/OrderStatus.java
@@ -1,10 +1,55 @@
 package com.buystuff.buystuff_api.enums;
 
+import java.util.Map;
+
 public enum OrderStatus {
-	PLACED,
-	SHIPPED,
+	// initiated by the system
 	PENDING_PAYMENT,
-	DELIVERED,
+	PLACED,
+	ACCEPTED,
+
+	// initiated by admin or user
 	CANCELLED,
-	RETURNED
+
+	// handled by the fulfillment manager
+	SHIPPED,
+	DELIVERED,
+	RETURNED;
+
+	private static final Map<OrderStatus, Map<OrderAction, OrderStatus>> TRANSITION_MAP = Map.of(
+		PENDING_PAYMENT, Map.of(
+			OrderAction.PAY, PLACED,
+			OrderAction.CANCEL, CANCELLED
+		),
+		PLACED, Map.of(
+			OrderAction.SHIP, SHIPPED,
+			OrderAction.CANCEL, CANCELLED
+		),	
+		SHIPPED, Map.of(
+			OrderAction.DELIVER, DELIVERED,
+			OrderAction.CANCEL, CANCELLED
+		),
+		DELIVERED, Map.of(
+			OrderAction.RETURN_PERIOD_OVER, ACCEPTED,
+			OrderAction.RETURN, RETURNED
+		)
+	);
+
+	public OrderStatus next(OrderAction action) {
+		if (!TRANSITION_MAP.containsKey(this)) {
+			throw new IllegalArgumentException(
+				String.format("No state transitions available on status: %s", this.toString())
+			);
+		}
+			
+		var possibleTransitions = TRANSITION_MAP.get(this);
+
+		if (!possibleTransitions.containsKey(action)) {
+			throw new IllegalArgumentException(
+				String.format("Invalid event %s on status: %s", action.toString(), this.toString())
+			);
+		}
+
+		return possibleTransitions.get(action);
+	}
 }
