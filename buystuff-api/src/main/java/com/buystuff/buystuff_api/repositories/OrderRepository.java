c140886796f7780b8d8915b5e7bdf54121f9712d
Prateik Lohani <lohaniprateik@gmail.com>
2026-02-20 17:32:49 +0530

    add: basic read methods

--- buystuff-api/src/main/java/com/buystuff/buystuff_api/repositories/OrderRepository.java
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/repositories/OrderRepository.java
@@ -1,13 +1,19 @@
 package com.buystuff.buystuff_api.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.buystuff.buystuff_api.entities.Account;
 import com.buystuff.buystuff_api.entities.Order;
+
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
-	
+	Optional<Order> findByOrderIdAndAccount_AccountId(UUID orderId, UUID accountId);
+	Page<Order> findByAccount(Account account, Pageable pageable);
 }
