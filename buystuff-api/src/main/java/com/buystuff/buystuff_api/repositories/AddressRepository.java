3831748cb2e8b20ae1a43f553a1722f0f3447514
Prateik Lohani <lohaniprateik@gmail.com>
2026-03-09 18:07:39 +0530

    add: find by account method

--- buystuff-api/src/main/java/com/buystuff/buystuff_api/repositories/AddressRepository.java
+++ buystuff-api/src/main/java/com/buystuff/buystuff_api/repositories/AddressRepository.java
@@ -1,14 +1,18 @@
 package com.buystuff.buystuff_api.repositories;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.buystuff.buystuff_api.entities.Account;
 import com.buystuff.buystuff_api.entities.Address;
+
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, UUID> {
 	Optional<Address> findByAddressIdAndAccount_AccountId(UUID addressId, UUID accountId);
+	List<Address> findByAccount(Account account);
 }
